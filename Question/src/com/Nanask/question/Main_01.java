5582a6b7f33bac3bf8e96027c1579441803162dd
Nanask <ybhag1005@daum.net>
2021-03-31 23:09:22 +0900

    beakjoon

---
+++ Question/src/com/Nanask/question/Main_01.java
@@ -0,0 +1,21 @@
+package com.Nanask.question;
+
+//1000번
+
+import java.util.Scanner;
+
+public class Main_01 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a;
+		int b;
+		
+		a = scan.nextInt();
+		b = scan.nextInt();
+		System.out.println(a + b);
+		
+	}
+}
---
+++ Question/src/com/Nanask/question/Main_02.java
@@ -0,0 +1,21 @@
+package com.Nanask.question;
+
+//1001번
+
+import java.util.Scanner;
+
+public class Main_02 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = 0;
+		int b = 0;
+		
+		a = scan.nextInt();
+		b = scan.nextInt();
+		
+		System.out.println(a - b);
+		
+	}
+
+}
